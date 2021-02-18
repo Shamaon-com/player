5708379b2c00e907d61f7e0e5acafb17dfb5c510
juanjose9889 <juanjosegarrido98@outlook.es>
2021-02-18 14:19:40 +0100

    init

---
+++ pages/[id].js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'
+import VideoPlayer from '../components/videoPlayer'
+
+export default function Home() {
+  const [sources,setSources] = useState([{id:1 , src:"source 1"}, {id:2 , src:"source 2"},{id:3 , src:"source 3"}, {id:4 , src:"source 4"},]);
+  const router = useRouter();
+  const[id, setId] = useState(null);
+  const [currentSrc ,setCurrentSrc] = useState({id:1 , src:"https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8"});
+
+  
+
+  useEffect(() => {
+
+    if(router.query.id !== undefined){
+      console.log(router.query.id)
+      const { id } = router.query;
+      setId(id);
+    }
+  },[router.query])
+
+  useEffect(()=> {
+    const newSrc = sources.map((item, index) => {
+      return{
+       id: item.id,
+       src: item.src + id,
+       }         
+     })
+     console.log(id)
+     console.log(newSrc)
+     setSources(newSrc);
+
+  }, [id]) 
+
+  const buildObject = () => {
+    return {
+      autoplay: true,
+      controls: true,
+      sources: [{
+        src: 'qwd',
+        type: 'application/x-mpegURL'
+      }]
+    }
+  }
+
+  const renderVideoPlayer = () =>{
+    
+    return (
+      <>
+      {currentSrc && <div> </div>  }
+      </>
+    )
+  }
+
+  return (
+    <div>
+      {renderVideoPlayer()}
+    </div>
+
+  )
+}
+
